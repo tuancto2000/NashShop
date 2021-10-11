60b0d59070adf93e5a2b1b149d2389dd94fcb8d4
Nguyen Xuan Tuan <58553278+tuancto2000@users.noreply.github.com>
2021-10-11 13:17:19 +0700

    add Dbcontext, product services

--- NashShop_BackendApi/Data/Entities/Product.cs
+++ NashShop_BackendApi/Data/Entities/Product.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NashShop_BackendApi.Model
+namespace NashShop_BackendApi.Data.Entities
 {
     public class Product
     {
