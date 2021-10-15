7d1f8dc57c78f54e9f74ada7b8db6af2e3098ac4
Nguyen Xuan Tuan <58553278+tuancto2000@users.noreply.github.com>
2021-10-15 21:40:07 +0700

    add dbcontext

--- NashShop_BackendApi/Startup.cs
+++ NashShop_BackendApi/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NashShop_BackendApi.Data.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +17,17 @@ namespace NashShop_BackendApi
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration configuration { get; }
+        public Startup(IConfiguration Configuration)
+        {
+            configuration = Configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddDbContext<NashShopDbContext>(
+        options => options.UseSqlServer(configuration.GetConnectionString("NashShopDb")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
